01a7fc3d099f7873692ee43ffe5882c9bf6de378
matheushso <matheushso99@hotmail.com>
2023-01-15 22:27:03 -0300

    create state controller

---
+++ src/main/java/com/food/delivery/api/controller/EstadoController.java
@@ -0,0 +1,24 @@
+package com.food.delivery.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.food.delivery.domain.model.Estado;
+import com.food.delivery.domain.repository.EstadoRepository;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@GetMapping
+	private List<Estado> listar() {
+		return estadoRepository.listar();
+	}
+}
