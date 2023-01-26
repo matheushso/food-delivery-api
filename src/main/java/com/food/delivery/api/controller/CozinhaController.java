210991f34adb95cf695e452a66189058e6b64809
matheushso <matheushso99@hotmail.com>
2023-01-25 22:42:44 -0300

    create new get kitchen from id path variable

--- src/main/java/com/food/delivery/api/controller/CozinhaController.java
+++ src/main/java/com/food/delivery/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class CozinhaController {
 	@GetMapping
 	private List<Cozinha> listar() {
 		return cozinhaRepository.listar();
+	}
+
+	@GetMapping("/{id}")
+	private Cozinha buscar(@PathVariable Long id) {
+		return cozinhaRepository.buscar(id);
 	}
 }
