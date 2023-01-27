5aedc4265a68a06ff71618ff9ffc630752736f80
matheushso <matheushso99@hotmail.com>
2023-01-26 22:35:48 -0300

    create methods post and put of kitchen

--- src/main/java/com/food/delivery/api/controller/CozinhaController.java
+++ src/main/java/com/food/delivery/api/controller/CozinhaController.java
@@ -2,10 +2,17 @@ package com.food.delivery.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.food.delivery.domain.model.Cozinha;
@@ -24,7 +31,33 @@ public class CozinhaController {
 	}
 
 	@GetMapping("/{id}")
-	private Cozinha buscar(@PathVariable Long id) {
-		return cozinhaRepository.buscar(id);
+	private ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+		Cozinha cozinha = cozinhaRepository.buscar(id);
+
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	private Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+
+	@PutMapping("/{id}")
+	private ResponseEntity<Cozinha> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtualizar = cozinhaRepository.buscar(id);
+
+		if (cozinhaAtualizar != null) {
+			BeanUtils.copyProperties(cozinha, cozinhaAtualizar, "id");
+
+			cozinhaAtualizar = cozinhaRepository.salvar(cozinhaAtualizar);
+			return ResponseEntity.ok(cozinhaAtualizar);
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 }
