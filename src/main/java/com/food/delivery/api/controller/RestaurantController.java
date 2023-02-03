a588670874d6476dc9d86036f31f32b81873ef95
matheushso <matheushso99@hotmail.com>
2023-02-02 21:48:26 -0300

    create restaurant controller

---
+++ src/main/java/com/food/delivery/api/controller/RestaurantController.java
@@ -0,0 +1,67 @@
+package com.food.delivery.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.food.delivery.domain.exception.EntityNotFoundException;
+import com.food.delivery.domain.model.Restaurant;
+import com.food.delivery.domain.service.RestaurantService;
+
+@RestController
+@RequestMapping("/restaurants")
+public class RestaurantController {
+
+	@Autowired
+	private RestaurantService restaurantService;
+
+	@GetMapping
+	private List<Restaurant> findAll() {
+		return restaurantService.findAll();
+	}
+
+	@GetMapping("/{id}")
+	private ResponseEntity<Restaurant> findById(@PathVariable Long id) {
+		try {
+			Restaurant restaurant = restaurantService.findById(id);
+			return ResponseEntity.ok(restaurant);
+		} catch (EntityNotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PostMapping
+	private ResponseEntity<?> save(@RequestBody Restaurant restaurant) {
+		try {
+			restaurant = restaurantService.save(restaurant);
+			return ResponseEntity.status(HttpStatus.CREATED).body(restaurant);
+		} catch (EntityNotFoundException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+
+	@PutMapping("/{id}")
+	private ResponseEntity<?> update(@PathVariable Long id, @RequestBody Restaurant restaurant) {
+		try {
+			Restaurant restaurantUpdated = restaurantService.update(id, restaurant);
+			if (restaurantUpdated != null) {
+				return ResponseEntity.ok(restaurantUpdated);
+			}
+
+			return ResponseEntity.notFound().build();
+
+		} catch (EntityNotFoundException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+
+}
