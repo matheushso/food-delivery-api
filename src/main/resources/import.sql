2c38cb75d6e62d77ca69ac5585a75c60fe766b7d
matheushso <matheushso99@hotmail.com>
2023-02-05 13:32:21 -0300

    adjusting name of columns

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,22 +1,22 @@
-insert into kitchen (id, nome) values(1, 'Tailandasa');
-insert into kitchen (id, nome) values(2, 'Indiana');
+insert into kitchen (id, name) values(1, 'Tailandasa');
+insert into kitchen (id, name) values(2, 'Indiana');
 
-insert into restaurant (nome, taxa_frete, cozinha_id) values('Thai Gourmet', 10, 1);
-insert into restaurant (nome, taxa_frete, cozinha_id) values('Thai Delivery', 9.50, 1);
-insert into restaurant (nome, taxa_frete, cozinha_id) values('Tuk Tuk Comida', 15, 2);
+insert into restaurant (name, freight_rate, kitchen_id) values('Thai Gourmet', 10, 1);
+insert into restaurant (name, freight_rate, kitchen_id) values('Thai Delivery', 9.50, 1);
+insert into restaurant (name, freight_rate, kitchen_id) values('Tuk Tuk Comida', 15, 2);
 
-insert into payment_method (descricao) values('Cartão de crédito');
-insert into payment_method (descricao) values('Cartão de débito');
-insert into payment_method (descricao) values('Pix');
+insert into payment_method (description) values('Cartão de crédito');
+insert into payment_method (description) values('Cartão de débito');
+insert into payment_method (description) values('Pix');
 
-insert into permission (nome, descricao) values ('Cadastrar produtos', 'Permite cadastrar produtos');
-insert into permission (nome, descricao) values ('Consultar produtos', 'Permite consultar produtos');
+insert into permission (name, description) values ('Cadastrar produtos', 'Permite cadastrar produtos');
+insert into permission (name, description) values ('Consultar produtos', 'Permite consultar produtos');
 
 
-insert into state (id, nome) values (1, 'Paraná');
-insert into state (id, nome) values (2, 'São Paulo');
+insert into state (id, name) values (1, 'Paraná');
+insert into state (id, name) values (2, 'São Paulo');
 
-insert into city (nome, estado_id) values ('Maringá', 1);
-insert into city (nome, estado_id) values ('Curitiba', 1);
-insert into city (nome, estado_id) values ('Garulhos', 2);
-insert into city (nome, estado_id) values ('Campinas', 2);
+insert into city (name, state_id) values ('Maringá', 1);
+insert into city (name, state_id) values ('Curitiba', 1);
+insert into city (name, state_id) values ('Garulhos', 2);
+insert into city (name, state_id) values ('Campinas', 2);
