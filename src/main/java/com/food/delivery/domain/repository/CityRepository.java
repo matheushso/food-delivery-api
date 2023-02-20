fd5bc4166961b44f9acc8b9807c318a7303f1a15
matheushso <matheushso99@hotmail.com>
2023-02-20 20:23:31 -0300

    use jpa repository implementation in city repository

--- src/main/java/com/food/delivery/domain/repository/CityRepository.java
+++ src/main/java/com/food/delivery/domain/repository/CityRepository.java
@@ -1,19 +1,11 @@
 package com.food.delivery.domain.repository;
 
-import java.util.List;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import com.food.delivery.domain.model.City;
 
 @Component
-public interface CityRepository {
+public interface CityRepository extends JpaRepository<City, Long> {
 
-	List<City> listar();
-
-	City buscar(Long id);
-
-	City salvar(City cidade);
-
-	void remover(City cidade);
 }
