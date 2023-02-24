75441f91cb1bb2d0b452c3fe4028f6c6c398784f
matheushso <matheushso99@hotmail.com>
2023-02-23 21:52:36 -0300

    adjusting annotation

--- src/main/java/com/food/delivery/domain/repository/CityRepository.java
+++ src/main/java/com/food/delivery/domain/repository/CityRepository.java
@@ -1,11 +1,11 @@
 package com.food.delivery.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.food.delivery.domain.model.City;
 
-@Component
+@Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 
 }
