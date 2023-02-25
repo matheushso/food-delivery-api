eb2530741c211a33eb4884e0437a23849a655cae
matheushso <matheushso99@hotmail.com>
2023-02-24 21:39:27 -0300

    create tests of kitchen controller

---
+++ src/test/java/com/food/delivery/KitchenControllerTests.java
@@ -0,0 +1,163 @@
+package com.food.delivery;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import com.food.delivery.domain.model.Kitchen;
+
+import io.restassured.http.ContentType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class KitchenControllerTests {
+
+	@LocalServerPort
+	private Integer port;
+
+	@Test
+	public void shouldReturnStatus200_WhenConsultKitchens() {
+		given()
+			.basePath("/kitchens")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
+
+	@Test
+	public void shouldReturnStatus200_WhenFindKitchenPerId() {
+		given()
+			.pathParam("id", 1)
+			.basePath("/kitchens/{id}")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
+
+	@Test
+	public void shouldReturnStatus404_WhenNotFoundKitchenPerId() {
+		given()
+			.pathParam("id", 9)
+			.basePath("/kitchens/{id}")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.NOT_FOUND.value());
+	}
+
+	@Test
+	public void shouldReturnStatus201_WhenCreatedKitchen() {
+		Kitchen kitchen = Kitchen.builder()
+				.name("Kitchen")
+				.build();
+
+		given()
+			.basePath("/kitchens")
+			.port(port)
+			.body(kitchen)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
+
+		Kitchen kitchenCreated = findById(3);
+
+		assertEquals(kitchen.getName(), kitchenCreated.getName());
+	}
+
+	@Test
+	public void shouldReturnStatus200_WhenUpdatedKitchen() {
+		Kitchen kitchenCreate = Kitchen.builder()
+				.name("Kitchen")
+				.build();
+
+		given()
+			.basePath("/kitchens")
+			.port(port)
+			.body(kitchenCreate)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
+
+		Kitchen kitchen = Kitchen.builder()
+				.name("Kitchen update")
+				.build();
+
+		given()
+			.pathParam("id", 3)
+			.basePath("/kitchens/{id}")
+			.port(port)
+			.body(kitchen)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.put()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+
+		Kitchen kitchenUpdated = findById(3);
+
+		assertEquals(kitchen.getName(), kitchenUpdated.getName());
+	}
+	
+	@Test
+	public void shouldReturnStatus404_WhenUpdatedKitchenWasNotFound() {
+		Kitchen kitchen = Kitchen.builder()
+				.name("Kitchen update")
+				.build();
+
+		given()
+			.pathParam("id", 9)
+			.basePath("/kitchens/{id}")
+			.port(port)
+			.body(kitchen)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.put()
+		.then()
+			.statusCode(HttpStatus.NOT_FOUND.value());
+	}
+	
+	@Test
+	public void shouldReturnStatus200_WhenDeletedKitchen() {
+		//TODO Fazer demais cenários de Delete
+		
+		given()
+			.pathParam("id", 3)
+			.basePath("/kitchens/{id}")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.delete()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
+	
+	private Kitchen findById(int id) {
+		return given()
+				.pathParam("id", id)
+				.basePath("/kitchens/{id}")
+				.port(port)
+				.accept(ContentType.JSON)
+			.when()
+				.get()
+				.as(Kitchen.class);
+	}
+}
