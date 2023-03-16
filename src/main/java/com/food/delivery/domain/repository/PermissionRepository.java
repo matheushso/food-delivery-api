8a7cf9b8c2a19900efe393f24122823f385328a5
matheushso <matheushso99@hotmail.com>
2023-03-15 21:55:52 -0300

    adjusting Permission repository to use JPA

--- src/main/java/com/food/delivery/domain/repository/PermissionRepository.java
+++ src/main/java/com/food/delivery/domain/repository/PermissionRepository.java
@@ -1,19 +1,9 @@
 package com.food.delivery.domain.repository;
 
-import java.util.List;
-
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.food.delivery.domain.model.Permission;
 
-@Component
-public interface PermissionRepository {
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
 
-	List<Permission> listar();
-
-	Permission buscar(Long id);
-
-	Permission salvar(Permission permissao);
-
-	void remover(Permission permissao);
 }
