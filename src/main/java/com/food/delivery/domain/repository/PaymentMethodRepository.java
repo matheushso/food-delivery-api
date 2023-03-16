e1253a6a13e02de9cb7acec25c91cdadb7dbe389
matheushso <matheushso99@hotmail.com>
2023-03-15 21:56:03 -0300

    adjusting Payment method repository to use JPA

--- src/main/java/com/food/delivery/domain/repository/PaymentMethodRepository.java
+++ src/main/java/com/food/delivery/domain/repository/PaymentMethodRepository.java
@@ -1,19 +1,9 @@
 package com.food.delivery.domain.repository;
 
-import java.util.List;
-
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.food.delivery.domain.model.PaymentMethod;
 
-@Component
-public interface PaymentMethodRepository {
+public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
 
-	List<PaymentMethod> listar();
-
-	PaymentMethod buscar(Long id);
-
-	PaymentMethod salvar(PaymentMethod formaPagamento);
-
-	void remover(PaymentMethod formaPagamento);
 }
