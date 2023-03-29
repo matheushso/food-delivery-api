afc73b34cb9683d40e8ff6dfef93d19a573dd685
matheushso <matheushso99@hotmail.com>
2023-03-29 18:10:14 -0300

    add many to many mapping from restaurant and payment methods

--- src/main/java/com/food/delivery/domain/model/Restaurant.java
+++ src/main/java/com/food/delivery/domain/model/Restaurant.java
@@ -1,12 +1,15 @@
 package com.food.delivery.domain.model;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -29,4 +32,8 @@ public class Restaurant {
 	@ManyToOne
 	@JoinColumn(nullable = false)
 	private Kitchen kitchen;
+	
+	@ManyToMany
+	@JoinTable(name = "restaurant_payment_method", inverseJoinColumns = @JoinColumn(name = "payment_method_id"))
+	private List<PaymentMethod> paymentMethods;
 }
