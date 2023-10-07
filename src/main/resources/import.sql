10c453ba1f2da7b51bc757473eb5327d7e67b7b7
Matheus Oliveira <matheushso99@hotmail.com>
2023-10-07 12:34:20 -0300

    add address to restaurant table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,15 @@
 insert into kitchen (id, name) values(1, 'Tailandesa');
 insert into kitchen (id, name) values(2, 'Indiana');
 
-insert into restaurant (name, freight_rate, kitchen_id) values('Thai Gourmet', 10, 1);
+insert into state (id, name) values (1, 'Paraná');
+insert into state (id, name) values (2, 'São Paulo');
+
+insert into city (name, state_id) values ('Maringá', 1);
+insert into city (name, state_id) values ('Curitiba', 1);
+insert into city (name, state_id) values ('Garulhos', 2);
+insert into city (name, state_id) values ('Campinas', 2);
+
+insert into restaurant (name, freight_rate, kitchen_id, address_city_id, address_zip_code, address_street, address_number, address_neighborhood) values ('Thai Gourmet', 10, 1, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
 insert into restaurant (name, freight_rate, kitchen_id) values('Thai Delivery', 9.50, 1);
 insert into restaurant (name, freight_rate, kitchen_id) values('Tuk Tuk Comida', 15, 2);
 
@@ -11,14 +19,5 @@ insert into payment_method (description) values('Pix');
 
 insert into permission (name, description) values ('Cadastrar produtos', 'Permite cadastrar produtos');
 insert into permission (name, description) values ('Consultar produtos', 'Permite consultar produtos');
-
-
-insert into state (id, name) values (1, 'Paraná');
-insert into state (id, name) values (2, 'São Paulo');
-
-insert into city (name, state_id) values ('Maringá', 1);
-insert into city (name, state_id) values ('Curitiba', 1);
-insert into city (name, state_id) values ('Garulhos', 2);
-insert into city (name, state_id) values ('Campinas', 2);
 
 insert into restaurant_payment_method (restaurant_id, payment_method_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
