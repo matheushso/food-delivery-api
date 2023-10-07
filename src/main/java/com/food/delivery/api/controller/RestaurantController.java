96ea681e7322f9cd9d24520ccc56f560f239618d
Matheus Oliveira <matheushso99@hotmail.com>
2023-10-07 12:35:07 -0300

    not update address when updating restaurant

--- src/main/java/com/food/delivery/api/controller/RestaurantController.java
+++ src/main/java/com/food/delivery/api/controller/RestaurantController.java
@@ -63,7 +63,7 @@ public class RestaurantController {
 		try {
 			Restaurant restaurantUpdate = restaurantService.findById(id);
 
-			BeanUtils.copyProperties(restaurant, restaurantUpdate, "id", "paymentMethods");
+			BeanUtils.copyProperties(restaurant, restaurantUpdate, "id", "paymentMethods", "address");
 
 			restaurantUpdate = restaurantService.save(restaurantUpdate);
 			return ResponseEntity.ok(restaurantUpdate);
