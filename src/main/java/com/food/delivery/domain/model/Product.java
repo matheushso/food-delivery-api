6aae99510af5d3e7c5608359b768c3bdd108ea1b
Matheus Oliveira <matheushso99@hotmail.com>
2023-10-18 20:13:17 -0300

    create product

---
+++ src/main/java/com/food/delivery/domain/model/Product.java
@@ -0,0 +1,29 @@
+package com.food.delivery.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Product {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private Boolean active;
+
+    @ManyToOne
+    private Restaurant restaurant;
+}
