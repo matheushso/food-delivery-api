0af8bee6cf97db851842207f0ee0be895dd0876e
Matheus Oliveira <matheushso99@hotmail.com>
2023-10-18 19:22:04 -0300

    create field to store restaurant creation and update date

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,9 +9,9 @@ insert into city (name, state_id) values ('Curitiba', 1);
 insert into city (name, state_id) values ('Garulhos', 2);
 insert into city (name, state_id) values ('Campinas', 2);
 
-insert into restaurant (name, freight_rate, kitchen_id, address_city_id, address_zip_code, address_street, address_number, address_neighborhood) values ('Thai Gourmet', 10, 1, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
-insert into restaurant (name, freight_rate, kitchen_id) values('Thai Delivery', 9.50, 1);
-insert into restaurant (name, freight_rate, kitchen_id) values('Tuk Tuk Comida', 15, 2);
+insert into restaurant (name, freight_rate, kitchen_id, creation_date, update_date, address_city_id, address_zip_code, address_street, address_number, address_neighborhood) values ('Thai Gourmet', 10, 1, utc_timestamp, utc_timestamp, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
+insert into restaurant (name, freight_rate, kitchen_id, creation_date, update_date) values('Thai Delivery', 9.50, 1, utc_timestamp, utc_timestamp);
+insert into restaurant (name, freight_rate, kitchen_id, creation_date, update_date) values('Tuk Tuk Comida', 15, 2, utc_timestamp, utc_timestamp);
 
 insert into payment_method (description) values('Cartão de crédito');
 insert into payment_method (description) values('Cartão de débito');
