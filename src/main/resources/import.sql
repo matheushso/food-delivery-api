b851dd549bf5690024c3d780a40f355a6e3fd984
Matheus Oliveira <matheushso99@hotmail.com>
2023-10-19 22:00:43 -0300

    add products

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -21,3 +21,9 @@ insert into permission (name, description) values ('Cadastrar produtos', 'Permit
 insert into permission (name, description) values ('Consultar produtos', 'Permite consultar produtos');
 
 insert into restaurant_payment_method (restaurant_id, payment_method_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
+
+insert into product (name, description, price, active, restaurant_id) values ('Massa Carbonara', 'Uma massa clássica italiana com bacon, ovos e queijo.', 15.99, true, 1);
+insert into product (name, description, price, active, restaurant_id) values ('Sushi Combo', 'Uma seleção de sushis variados com acompanhamentos.', 22.99, true, 2);
+insert into product (name, description, price, active, restaurant_id) values ('Pizza Margherita', 'Uma pizza tradicional com molho de tomate, muçarela e manjericão.', 14.99, true, 2);
+insert into product (name, description, price, active, restaurant_id) values ('Hambúrguer Gourmet', 'Um hambúrguer suculento com ingredientes gourmet.', 12.99, true, 3);
+insert into product (name, description, price, active, restaurant_id) values ('Salada Caesar', 'Uma salada refrescante com alface, croutons e molho Caesar.', 8.99, true, 3);
