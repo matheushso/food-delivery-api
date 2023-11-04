71da5e71ddc965ef9610491f6dc6d6c8c0ed5984
Matheus Oliveira <matheushso99@hotmail.com>
2023-11-04 09:18:52 -0300

    adjust the size of table columns

--- src/main/resources/db/migration/V005__create_restaurant_table.sql
+++ src/main/resources/db/migration/V005__create_restaurant_table.sql
@@ -4,14 +4,15 @@ create table restaurant
     name                 varchar(80)    not null,
     freight_rate         decimal(19, 2) not null,
     kitchen_id           bigint         not null,
-    address_zip_code     varchar(8)     not null,
-    address_street       varchar(80)    not null,
-    address_number       varchar(10)    not null,
-    address_complement   varchar(100),
-    address_neighborhood varchar(80)    not null,
-    address_city_id      bigint         not null,
     creation_date        datetime(6) not null,
-    update_date          datetime(6),
+    update_date          datetime(6) not null,
+
+    address_city_id      bigint,
+    address_zip_code     varchar(9),
+    address_street       varchar(100),
+    address_number       varchar(20),
+    address_complement   varchar(60),
+    address_neighborhood varchar(60),
 
     primary key (id)
 ) engine=InnoDB default charset=utf8mb4;
