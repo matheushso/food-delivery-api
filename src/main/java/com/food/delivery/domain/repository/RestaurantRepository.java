4cabe58f4d948f282ffa801f90ba96be24d64d90
Matheus Oliveira <matheushso99@hotmail.com>
2023-11-04 09:26:38 -0300

    remove payment methods in restaurant json return

--- src/main/java/com/food/delivery/domain/repository/RestaurantRepository.java
+++ src/main/java/com/food/delivery/domain/repository/RestaurantRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
-    @Query("select distinct r from Restaurant r join fetch r.kitchen left join fetch r.paymentMethods")
+    @Query("select distinct r from Restaurant r join fetch r.kitchen")
     List<Restaurant> findAll();
 }
