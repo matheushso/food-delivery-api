516f007a29de7893ec296d04583ca01fa0a28689
Matheus Oliveira <matheushso99@hotmail.com>
2023-11-26 13:18:13 -0300

    simplify validation and return of HttpStatus

--- src/main/java/com/food/delivery/api/controller/CityController.java
+++ src/main/java/com/food/delivery/api/controller/CityController.java
@@ -1,83 +1,49 @@
 package com.food.delivery.api.controller;
 
-import java.util.List;
-
+import com.food.delivery.domain.model.City;
+import com.food.delivery.domain.service.CityService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.food.delivery.domain.exception.EntityNotFoundException;
-import com.food.delivery.domain.model.City;
-import com.food.delivery.domain.service.CityService;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cities")
 public class CityController {
 
-	@Autowired
-	private CityService cityService;
+    @Autowired
+    private CityService cityService;
 
-	@GetMapping
-	private List<City> findAll() {
-		return cityService.findAll();
-	}
+    @GetMapping
+    private List<City> findAll() {
+        return cityService.findAll();
+    }
 
-	@GetMapping("/{id}")
-	private ResponseEntity<?> findById(@PathVariable Long id) {
-		try {
-			City city = cityService.findById(id);
-			return ResponseEntity.ok(city);
+    @GetMapping("/{id}")
+    private City findById(@PathVariable Long id) {
+        return cityService.findById(id);
+    }
 
-		} catch (EntityNotFoundException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-		}
-	}
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private City save(@RequestBody City city) {
+        return cityService.save(city);
+    }
 
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	private ResponseEntity<?> save(@RequestBody City city) {
-		try {
-			city = cityService.save(city);
-			return ResponseEntity.status(HttpStatus.CREATED).body(city);
+    @PutMapping("/{id}")
+    private City update(@PathVariable Long id, @RequestBody City city) {
+        City cityUpdate = cityService.findById(id);
 
-		} catch (EntityNotFoundException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-		}
-	}
+        BeanUtils.copyProperties(city, cityUpdate, "id");
 
-	@PutMapping("/{id}")
-	private ResponseEntity<?> update(@PathVariable Long id, @RequestBody City city) {
-		try {
-			City cityUpdate = cityService.findById(id);
+        return cityService.save(cityUpdate);
+    }
 
-			BeanUtils.copyProperties(city, cityUpdate, "id");
-
-			cityUpdate = cityService.save(cityUpdate);
-			return ResponseEntity.ok(cityUpdate);
-
-		} catch (EntityNotFoundException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-		}
-	}
-
-	@DeleteMapping("/{id}")
-	private ResponseEntity<?> delete(@PathVariable Long id) {
-		try {
-			cityService.delete(id);
-			return ResponseEntity.noContent().build();
-
-		} catch (EntityNotFoundException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-		}
-	}
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    private void delete(@PathVariable Long id) {
+        cityService.delete(id);
+    }
 }
