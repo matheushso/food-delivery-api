d82b6114e4602d87b9521bee9b8fb8e817b0cf1a
Matheus Oliveira <matheushso99@hotmail.com>
2024-02-25 16:38:51 -0300

    extend from response entity exception handler class

--- src/main/java/com/food/delivery/api/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/food/delivery/api/exceptionhandler/GlobalExceptionHandler.java
@@ -3,29 +3,43 @@ package com.food.delivery.api.exceptionhandler;
 import com.food.delivery.domain.exception.BusinessException;
 import com.food.delivery.domain.exception.EntityInUseException;
 import com.food.delivery.domain.exception.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> getEntityNotFoundException(EntityNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ErrorMessage(ex.getMessage()));
+    public ResponseEntity<?> getEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(),
+                HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(EntityInUseException.class)
-    public ResponseEntity<?> getEntityInUseException(EntityInUseException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(new ErrorMessage(ex.getMessage()));
+    public ResponseEntity<?> getEntityInUseException(EntityInUseException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(),
+                HttpStatus.CONFLICT, request);
     }
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<?> getBusinessException(BusinessException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorMessage(ex.getMessage()));
+    public ResponseEntity<?> getBusinessException(BusinessException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(),
+                HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        if (body == null) {
+            body = new ErrorMessage(status.getReasonPhrase());
+        } else if (body instanceof String) {
+            body = new ErrorMessage((String) body);
+        }
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 }
